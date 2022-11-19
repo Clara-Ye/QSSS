9354d1eea61ea43b79672063d2e4422396057ff2
Clara Ye <zixuany@andrew.cmu.edu>
2022-11-19 15:28:30 -0500

    updated parsing script; proceeded with coding

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # QSSS
 This is the repository for my QSSS senior thesis.
+
+# Known issues:
+## Script:
+- The parser splits at abbreviations like 'etc.' 'vs.' 'i.e.' because of the period
+## Coding:
+- Last sentence in the report RQSig or IntImp...?
+- Discard section titles?
--- parse_files.py
+++ parse_files.py
@@ -1,8 +1,14 @@
 import os
 from pathlib import Path
-from nltk import tokenize
+import re
 import pandas as pd
 from tqdm import tqdm
+
+SPLIT_PUNC = '[!.?;] |\n|\.["”\)] '
+STRIP_PUNC = '!.?;:'
+
+def strip_punc(sent):
+    return sent.strip(STRIP_PUNC)
 
 def parse_doc_all(data_dir, save_dir):
     path_list = Path(data_dir).rglob('*.txt')
@@ -11,16 +17,16 @@ def parse_doc_all(data_dir, save_dir):
         f = open(file_path, "r", encoding="utf8")
         file_text = f.read()
         f.close()
-        # split text by sentences
-        file_text = file_text.replace("\n", " ")
-        sent = tokenize.sent_tokenize(file_text)
+        # split text by sentences and clean up trailing punctuations
+        sent = re.split(SPLIT_PUNC, file_text)
+        sent = list(map(strip_punc, sent))
         # make dataframe of text and write into csv
-        df = pd.DataFrame({'sentence': sent, 'tag1': None, 'tag2': None, 'section': None})
+        df = pd.DataFrame({'sentence': sent, 'tag1': None, 'tag2': None, 'tag3': None, 'section': None, 'notes': None})
         file_name = os.path.split(file_path)[1][:-4]
         df.to_csv('{}\{}.csv'.format(save_dir, file_name), index=False, encoding='utf-8-sig') 
 
 if __name__ == "__main__":
     current_dir = os.getcwd()
-    data_dir = os.path.join(current_dir, 'data\\raw')
+    data_dir = os.path.join(current_dir, 'data\\raw\\extra')
     save_dir = os.path.join(current_dir, 'data\\tag')
     parse_doc_all(data_dir, save_dir)
