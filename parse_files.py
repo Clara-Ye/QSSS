1d2520785095a40a1d13d9a83d776829b27278fe
Clara Ye <zixuany@andrew.cmu.edu>
2022-11-22 13:25:13 -0500

    parsing script now skips certain abbreviations; proceeded with tagging

--- parse_files.py
+++ parse_files.py
@@ -4,8 +4,25 @@ import re
 import pandas as pd
 from tqdm import tqdm
 
-SPLIT_PUNC = '[!.?;] |\n|\.["”\)] '
+# Not to split on pattens of "these words followed by period"
+SKIP_LIST = ['eg', 'e.g', 'ie', 'i.e', 'et al', 'etc', 'ibid',
+             'vs', 'v.s', 'VS', 'V.S', 'versus',
+             'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+             'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+             'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun',
+             'Dr', 'Prof', 'Mr',  'Ms', 'Mrs', 'Miss',
+             'US', 'U.S', 'UK', 'U.K', 'EU', 'E.U']
+SKIP_REGEX = ''.join(['(?<!{})'.format(s) for s in SKIP_LIST])
+# Split on ! or . or ? or ; followed by a space
+# the above patterns followed by " or ” or ) followed by a space
+# or newline
+SPLIT_REGEX = SKIP_REGEX + '[!.?;] |[!.?;]["”\)] |\n'
+# Strip these trailing punctuations:
 STRIP_PUNC = '!.?;:'
+
+# Paths FROM THE CURRENT DIRECTORY to data and save locations
+DATA_PATH = 'data\\raw'
+SAVE_PATH = 'data\\tag'
 
 def strip_punc(sent):
     return sent.strip(STRIP_PUNC)
@@ -18,7 +35,7 @@ def parse_doc_all(data_dir, save_dir):
         file_text = f.read()
         f.close()
         # split text by sentences and clean up trailing punctuations
-        sent = re.split(SPLIT_PUNC, file_text)
+        sent = re.split(SPLIT_REGEX, file_text)
         sent = list(map(strip_punc, sent))
         # make dataframe of text and write into csv
         df = pd.DataFrame({'sentence': sent, 'tag1': None, 'tag2': None, 'tag3': None, 'section': None, 'notes': None})
@@ -27,6 +44,6 @@ def parse_doc_all(data_dir, save_dir):
 
 if __name__ == "__main__":
     current_dir = os.getcwd()
-    data_dir = os.path.join(current_dir, 'data\\raw\\extra')
-    save_dir = os.path.join(current_dir, 'data\\tag')
+    data_dir = os.path.join(current_dir, DATA_PATH)
+    save_dir = os.path.join(current_dir, SAVE_PATH)
     parse_doc_all(data_dir, save_dir)
